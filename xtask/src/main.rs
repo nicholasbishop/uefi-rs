de82013c300dc6bbdc606c661ccc8adc7a0a5d81
Nicholas Bishop <nicholasbishop@google.com>
2021-08-30 00:24:46 -0400

    xtask: move UefiTarget to main

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -2,9 +2,32 @@ mod cargo;
 mod qemu;
 
 use anyhow::Error;
-use cargo::{run_cargo, CargoCommand, Package, UefiTarget};
+use cargo::{run_cargo, CargoCommand, Package};
 use qemu::run_qemu_test;
 use structopt::StructOpt;
+
+/// UEFI target to build for.
+#[derive(Clone, Copy, strum::Display, strum::EnumString)]
+#[strum(serialize_all = "snake_case")]
+pub enum UefiTarget {
+    Aarch64,
+    X86_64,
+}
+
+impl Default for UefiTarget {
+    fn default() -> Self {
+        Self::X86_64
+    }
+}
+
+impl UefiTarget {
+    pub fn target_triple(&self) -> &str {
+        match self {
+            Self::Aarch64 => "aarch64-unknown-uefi",
+            Self::X86_64 => "x86_64-unknown-uefi",
+        }
+    }
+}
 
 /// Task runner for uefi.
 #[derive(StructOpt)]
