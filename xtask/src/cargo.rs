f214d61178f204f2048d2769393d953d44b8f925
Nicholas Bishop <nbishop@nbishop.net>
2022-04-10 00:43:16 -0400

    Add xtask command to run uefi-test-runner natively

--- xtask/src/cargo.rs
+++ xtask/src/cargo.rs
@@ -68,6 +68,7 @@ pub enum Feature {
     // `uefi-test-runner` features.
     DebugSupport,
     MultiProcessor,
+    Native,
     Pxe,
     TestUnstable,
     TpmV1,
@@ -89,6 +90,7 @@ impl Feature {
 
             Self::DebugSupport => "uefi-test-runner/debug_support",
             Self::MultiProcessor => "uefi-test-runner/multi_processor",
+            Self::Native => "uefi-test-runner/native",
             Self::Pxe => "uefi-test-runner/pxe",
             Self::TestUnstable => "uefi-test-runner/unstable",
             Self::TpmV1 => "uefi-test-runner/tpm_v1",
@@ -202,6 +204,8 @@ pub enum CargoAction {
         document_private_items: bool,
     },
     Miri,
+    MiriRun,
+    Run,
     Test,
 }
 
@@ -240,6 +244,7 @@ pub struct Cargo {
     pub target: Option<UefiArch>,
     pub warnings_as_errors: bool,
     pub target_types: TargetTypes,
+    pub default_features: bool,
 }
 
 impl Cargo {
@@ -283,6 +288,19 @@ impl Cargo {
                 sub_action = Some("test");
                 cmd.env("MIRIFLAGS", "-Zmiri-strict-provenance");
             }
+            CargoAction::MiriRun => {
+                action = "miri";
+                sub_action = Some("run");
+                // TODO
+                cmd.env(
+                    "MIRIFLAGS",
+                    "-Zmiri-permissive-provenance -Zmiri-tree-borrows",
+                    //"-Zmiri-permissive-provenance",
+                );
+            }
+            CargoAction::Run => {
+                action = "run";
+            }
             CargoAction::Test => {
                 action = "test";
             }
@@ -294,6 +312,10 @@ impl Cargo {
 
         if self.release {
             cmd.arg("--release");
+        }
+
+        if !self.default_features {
+            cmd.arg("--no-default-features");
         }
 
         if let Some(target) = self.target {
