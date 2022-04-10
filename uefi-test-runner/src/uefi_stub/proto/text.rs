fcd9d0e6b9cc2959849f4b61ca94bb67ec2b634e
Nicholas Bishop <nbishop@nbishop.net>
2022-04-10 17:42:27 -0400

    Implement more of the UEFI stub
    Run with miri w/ tree borrows:
    
    cargo +nightly miri run --no-default-features --package uefi-test-runner --features uefi-test-runner/native

---
+++ uefi-test-runner/src/uefi_stub/proto/text.rs
@@ -0,0 +1,141 @@
+use crate::uefi_stub::boot::install_protocol_interface;
+use crate::uefi_stub::install_protocol_simple;
+use std::ptr;
+use uefi::{CStr16, Result, Status, StatusExt};
+use uefi_raw::protocol::console::{
+    InputKey, SimpleTextInputProtocol, SimpleTextOutputMode, SimpleTextOutputProtocol,
+};
+use uefi_raw::table::boot::InterfaceType;
+use uefi_raw::{Char16, Handle};
+
+unsafe extern "efiapi" fn reset_input(
+    this: *mut SimpleTextInputProtocol,
+    extended_verification: bool,
+) -> Status {
+    Status::SUCCESS
+}
+
+unsafe extern "efiapi" fn read_key_stroke(
+    this: *mut SimpleTextInputProtocol,
+    key: *mut InputKey,
+) -> Status {
+    Status::SUCCESS
+}
+
+pub fn install_input_protocol() -> Result<Handle> {
+    let interface = Box::new(SimpleTextInputProtocol {
+        reset: reset_input,
+        read_key_stroke,
+        wait_for_key: ptr::null_mut(),
+    });
+    // TODO: leak
+    let interface: *const _ = Box::leak(interface);
+
+    install_protocol_simple(None, &SimpleTextInputProtocol::GUID, interface.cast())
+}
+
+extern "efiapi" fn reset(this: *mut SimpleTextOutputProtocol, extended: bool) -> Status {
+    Status::SUCCESS
+}
+
+unsafe extern "efiapi" fn output_string(
+    this: *mut SimpleTextOutputProtocol,
+    string: *const Char16,
+) -> Status {
+    let s = CStr16::from_ptr(string.cast());
+    print!("{}", s.to_string());
+    Status::SUCCESS
+}
+
+unsafe extern "efiapi" fn test_string(
+    this: *mut SimpleTextOutputProtocol,
+    string: *const Char16,
+) -> Status {
+    todo!()
+}
+
+unsafe extern "efiapi" fn query_mode(
+    this: *mut SimpleTextOutputProtocol,
+    mode: usize,
+    columns: *mut usize,
+    rows: *mut usize,
+) -> Status {
+    *columns = 80;
+    *rows = 25;
+    Status::SUCCESS
+}
+
+extern "efiapi" fn set_mode(this: *mut SimpleTextOutputProtocol, mode: usize) -> Status {
+    Status::SUCCESS
+}
+
+extern "efiapi" fn set_attribute(this: *mut SimpleTextOutputProtocol, attribute: usize) -> Status {
+    Status::SUCCESS
+}
+
+extern "efiapi" fn clear_screen(this: *mut SimpleTextOutputProtocol) -> Status {
+    Status::SUCCESS
+}
+
+extern "efiapi" fn set_cursor_position(
+    this: *mut SimpleTextOutputProtocol,
+    column: usize,
+    row: usize,
+) -> Status {
+    Status::SUCCESS
+}
+
+extern "efiapi" fn enable_cursor(this: *mut SimpleTextOutputProtocol, visible: bool) -> Status {
+    Status::SUCCESS
+}
+
+#[repr(C)]
+struct SimpleTextOutput {
+    // TODO: signature
+    mode: SimpleTextOutputMode,
+    interface: SimpleTextOutputProtocol,
+}
+
+pub fn install_output_protocol() -> Result<Handle> {
+    let data = SimpleTextOutputMode {
+        max_mode: 1,
+        mode: 0,
+        attribute: 0,
+        cursor_column: 0,
+        cursor_row: 0,
+        cursor_visible: false,
+    };
+    let interface = SimpleTextOutputProtocol {
+        reset,
+        output_string,
+        test_string,
+        query_mode,
+        set_mode,
+        set_attribute,
+        clear_screen,
+        set_cursor_position,
+        enable_cursor,
+        mode: ptr::null_mut(),
+    };
+
+    // TODO
+    let p2 = Box::new(SimpleTextOutput {
+        mode: data,
+        interface: interface,
+    });
+    let p2 = Box::leak(p2);
+    p2.interface.mode = ptr::addr_of_mut!(p2.mode);
+
+    // TODO
+
+    let mut handle = ptr::null_mut();
+    unsafe {
+        install_protocol_interface(
+            &mut handle,
+            &SimpleTextOutputProtocol::GUID,
+            InterfaceType::NATIVE_INTERFACE,
+            ptr::addr_of_mut!(p2.interface).cast(),
+        )
+    }
+    .to_result_with_val(|| handle)
+}
--- uefi-test-runner/src/uefi_stub/uefi_services.rs
+++ uefi-test-runner/src/uefi_stub/uefi_services.rs
@@ -1,6 +1,47 @@
-use uefi::prelude::*;
+// TODO, copying a lot of stuff from uefi-services for now
 
-pub fn init(_st: &mut SystemTable<Boot>) -> uefi::Result {
+use core::ffi::c_void;
+use core::ptr::NonNull;
+use uefi::table::boot::{EventType, Tpl};
+use uefi::table::{Boot, SystemTable};
+use uefi::{Event, Status, StatusExt};
+
+/// Global logger object
+static LOGGER: uefi::logger::Logger = uefi::logger::Logger::new();
+
+/// Set up logging
+///
+/// This is unsafe because you must arrange for the logger to be reset with
+/// disable() on exit from UEFI boot services.
+pub unsafe fn init_logger(st: &mut SystemTable<Boot>) {
+    LOGGER.set_output(st.stdout());
+
+    // Set the logger.
+    log::set_logger(&LOGGER).unwrap(); // Can only fail if already initialized.
+
+    // Set logger max level to level specified by log features
+    log::set_max_level(log::STATIC_MAX_LEVEL);
+
+    // Schedule logger to be disabled on exit from UEFI boot services
+    st.boot_services()
+        .create_event(
+            EventType::SIGNAL_EXIT_BOOT_SERVICES,
+            Tpl::NOTIFY,
+            Some(exit_boot_services),
+            None,
+        )
+        .unwrap();
+}
+
+/// Notify the utility library that boot services are not safe to call anymore
+/// As this is a callback, it must be `extern "efiapi"`.
+unsafe extern "efiapi" fn exit_boot_services(_e: Event, _ctx: Option<NonNull<c_void>>) {
+    LOGGER.disable();
+}
+
+pub fn init(st: &mut SystemTable<Boot>) -> uefi::Result {
+    unsafe { init_logger(st) };
+
     Status::SUCCESS.to_result()
     // TODO
 }
