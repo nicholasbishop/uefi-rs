1a35085d50bcf8adcde54c387bb2c97de4f7ca2d
Nicholas Bishop <nbishop@nbishop.net>
2022-04-10 22:26:11 -0400

    Handle: impl Hash and Eq

--- src/data_types/mod.rs
+++ src/data_types/mod.rs
@@ -7,7 +7,7 @@ use core::{ffi::c_void, ptr::NonNull};
 /// Opaque handle to an UEFI entity (protocol, image...), guaranteed to be non-null.
 ///
 /// If you need to have a nullable handle (for a custom UEFI FFI for example) use `Option<Handle>`.
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, Hash, Eq, PartialEq)]
 #[repr(transparent)]
 pub struct Handle(NonNull<c_void>);
 
