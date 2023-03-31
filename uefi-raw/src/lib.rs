8538cc8cd9b678bedacafa454aa88738a7f5c787
Nicholas Bishop <nbishop@nbishop.net>
2023-03-31 01:15:31 -0400

    uefi-raw: Add empty package
    Also add a dep on the new package in `uefi`.

---
+++ uefi-raw/src/lib.rs
@@ -0,0 +1,16 @@
+//! Raw interface for working with UEFI.
+//!
+//! This crate is intended for implementing UEFI services. It is also used for
+//! implementing the [`uefi`] crate, which provides a safe wrapper around UEFI.
+//!
+//! For creating UEFI applications and drivers, consider using the [`uefi`]
+//! crate instead of `uefi-raw`.
+//!
+//! [`uefi`]: https://crates.io/crates/uefi
+
+#![no_std]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+// Enable some additional warnings and lints.
+#![deny(clippy::ptr_as_ptr, unused)]
+#![deny(clippy::all)]
+#![deny(clippy::must_use_candidate)]
