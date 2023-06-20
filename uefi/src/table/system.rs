67f91d0b8bfdc3f8919778a4b2609557105de682
Nicholas Bishop <nbishop@nbishop.net>
2023-06-19 23:29:51 -0400

    uefi: Simplify SystemTable::from_ptr
    Notably, drop the ptr->reference.

--- uefi/src/table/system.rs
+++ uefi/src/table/system.rs
@@ -1,6 +1,5 @@
 use core::ffi::c_void;
 use core::marker::PhantomData;
-use core::ptr::NonNull;
 use core::slice;
 
 use crate::proto::console::text;
@@ -108,8 +107,8 @@ impl<View: SystemTableView> SystemTable<View> {
     /// is valid. Otherwise, further operations on the object might result in
     /// undefined behaviour, even if the methods aren't marked as unsafe.
     pub unsafe fn from_ptr(ptr: *mut c_void) -> Option<Self> {
-        NonNull::new(ptr.cast()).map(|ptr| Self {
-            table: ptr.as_ref(),
+        (!ptr.is_null()).then(|| Self {
+            table: ptr.cast(),
             _marker: PhantomData,
         })
     }
